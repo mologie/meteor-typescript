f98a70d8c618a80dcec3b44f4184b7abb92b9a51
Oliver Kuckertz <oliver.kuckertz@mologie.de>
2015-03-15 06:12:52 +0100

    Update package.js for publishing on Atmosphere

--- package.js
+++ package.js
@@ -1,12 +1,13 @@
 Package.describe({
 	name: "mologie:typescript",
 	summary: "TypeScript integration for Meteor",
+	git: "https://github.com/mologie/meteor-typescript.git",
 	version: "0.0.1"
 });
 
 Package.registerBuildPlugin({
 	name: "compile-typescript",
-	use: ["underscore"],
+	use: ["underscore@1.0.0"],
 	sources: ["plugin/compile-typescript.js"],
 	npmDependencies: {
 		"typescript": "1.4.1"
