2754f444dbb11ad068751375aeb9d06e65f24521
Oliver Kuckertz <oliver.kuckertz@mologie.de>
2015-03-18 12:50:19 +0100

    Bump version to 0.0.5

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
 	name: "mologie:typescript",
 	summary: "TypeScript integration for Meteor",
 	git: "https://github.com/mologie/meteor-typescript.git",
-	version: "0.0.4"
+	version: "0.0.5"
 });
 
 var meteorPackageList = [
